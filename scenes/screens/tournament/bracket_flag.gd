53aa3014da6a6fe21847e4388a244feb3ad4d543
salty-max <max@jellycat.fr>
2026-01-28 00:35:28 +0100

    added tournament screen

---
+++ scenes/screens/tournament/bracket_flag.gd
@@ -0,0 +1,18 @@
+class_name BracketFlag
+extends TextureRect
+
+@onready var score_label: Label = %ScoreLabel
+@onready var border: TextureRect = %Border
+
+
+func set_as_current_team() -> void:
+	border.visible = true
+	
+	
+func set_as_winner(score: String) -> void:
+	score_label.text = score
+	score_label.visible = true
+	
+	
+func set_as_loser() -> void:
+	modulate = Color(0.2, 0.2, 0.2, 1)
